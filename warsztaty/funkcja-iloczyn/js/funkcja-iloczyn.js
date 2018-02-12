fc4fea640303f50b9655b910c12b16a954e6699c
Paweł Białek <pawelbialek23@op.pl>
2018-02-12 18:19:09 +0100

    warsztaty

---
+++ warsztaty/funkcja-iloczyn/js/funkcja-iloczyn.js
@@ -0,0 +1,16 @@
+function wykonajDzialanie( parametr1, parametr2, parametr3 ) {
+    var parametr1 = 2;
+    var parametr2 = 6;
+    var parametr3 = 3;
+    var rezultat = parametr1 + parametr2 - parametr3;
+    return rezultat;
+
+}
+//
+//wykonajDzialanie( 6, 5, 5);
+//
+//console.log(wykonajDzialanie);
+
+var koncowyEfekt = wykonajDzialanie();
+
+console.log(koncowyEfekt);
