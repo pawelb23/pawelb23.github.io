0fbb9e756014db325f196243c9e02d13c63dcbbc
Paweł Białek <pawelbialek23@op.pl>
2018-08-13 02:29:00 +0200

    dodana informacja o wierszu 35 w JS

--- warsztaty/js-ogloszenie-dwa/js/main.js
+++ warsztaty/js-ogloszenie-dwa/js/main.js
@@ -9,12 +9,11 @@ class Ogloszenie {
         this.cena = cena;
         this.aktywne = aktywne;
     }
-    
+
     czyAktywne() {
-        if(this.aktywne) {
+        if (this.aktywne) {
             console.log('jest aktywne');
-        }
-        else {
+        } else {
             console.log('nieaktywne');
         }
     }
@@ -24,7 +23,7 @@ var ogloszenie2 = new Ogloszenie(2, 'motor', 2000, false);
 
 var ogloszenie3 = new Ogloszenie(3, 'krzeslo', 100, true);
 
-var ogloszenia = [ new Ogloszenie(1, 'samochod', 1000, true),ogloszenie2];
+var ogloszenia = [new Ogloszenie(1, 'samochod', 1000, true), ogloszenie2];
 
 var ogloszenia2 = [ogloszenie2, ogloszenie3];
 
@@ -34,13 +33,13 @@ console.log(ogloszenia);
 
 ogloszenie2.czyAktywne();
 
-console.log('powyższe nieaktywne odnosi się do lini 35 w JavaScript --- poniższe trzy zaś do funkcji');
+console.log('powyższe nieaktywne odnosi się do lini 35 w JavaScript czyli [ogloszenie2.czyAktywne();] --- poniższe trzy zaś do funkcji');
 
-function wyswietlCzyAktywne(tabOgloszenia) { 
-    for(var i = 0; i < tabOgloszenia.length; i++) {
-        
-        console.log(tabOgloszenia.length);//ilość ogłoszeń - w sumie są trzy
-      
+function wyswietlCzyAktywne(tabOgloszenia) {
+    for (var i = 0; i < tabOgloszenia.length; i++) {
+
+        console.log(tabOgloszenia.length); //ilość ogłoszeń - w sumie są trzy
+
         tabOgloszenia[i].czyAktywne();
     }
 }
@@ -50,19 +49,19 @@ wyswietlCzyAktywne(ogloszenia);
 
 function iloscOgloszen(tablicaOgl) {
     var licznikAktywnych = 0;
-    for(var i = 0; i < tablicaOgl.length; i++) {
-       
-        console.log(tablicaOgl.length);//ilość pojawi się dopiero kiedy określimy o którą tablicę nam chodzi poniżej (czyli np. ogloszenia, albo ogloszenia2).
-        
-        
-        if(tablicaOgl[i].aktywne == true) {
+    for (var i = 0; i < tablicaOgl.length; i++) {
+
+        console.log(tablicaOgl.length); //ilość pojawi się dopiero kiedy określimy o którą tablicę nam chodzi poniżej (czyli np. ogloszenia, albo ogloszenia2).
+
+
+        if (tablicaOgl[i].aktywne == true) {
             licznikAktywnych++;
-        } 
-        
-//        console.log(tablicaOgl[i].aktywny);
-    }    
-//    console.log(tablicaOgl.length);
-//    console.log(licznikAktywnych);
+        }
+
+        //        console.log(tablicaOgl[i].aktywny);
+    }
+    //    console.log(tablicaOgl.length);
+    //    console.log(licznikAktywnych);
     return [licznikAktywnych, tablicaOgl.length - licznikAktywnych];
     //tablicaOgl.length - licznikAktywnych] z tej drugiej części wynika ilość nieaktywnych, gdyż 'tablicaOgl.length' to wszystkie ogłoszenia czyli 3 i od tego odejmujęmy 'licznikAktywnych' czyli aktywne ogłoszenia,ich ilość wynosi 2. Z powyższego zapisu wynika, że nieaktywne ogłoszenia to 3 - 2 = 1.
 }
